4266f0e11e29a0a5acee4701916153f99153e770
Vashee khan <vashee.khan.lits@laxmi.edu.in>
2025-07-14 20:09:42 +0530

    added a friend request model and sending freind request api

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../model/FriendRequest.js";
 import User from "../model/User.js";
 
 export async function getRecommendedUsers(req,res){
@@ -30,3 +31,45 @@ export async function getMyFriends(req,res){
         res.status(500).json({message:"Something went wrong "});
     }
 };
+export async function sendFriendRequest(req,res){
+ try {  const myId=req.user.id;
+    const {id:recipientId}=req.params;
+    //prevent sending request to yourself
+    if(myId==recipientId)return res.status(400).json({message:"you cannot send friend request to your self"});
+    const recipient=await User.findById(recipientId);
+
+    //check if recipient is existing or not
+    if(!recipient){
+        return res.status(400).json({message:"Recipient not found"});
+    }
+    //check if we are already freinds;
+    if(recipient.friends.includes(myId)){
+        return res.status(400).json({message:"we are already friends"});
+    }
+    //check if a req already exist
+    const existingRequest=await FriendRequest.findOne({
+        $or:[
+            {
+                sender:myId,
+                recipient:recipientId
+            },{
+                sender:recipientId,
+                recipient:senderId
+            }
+        ]
+    })
+    if(existingRequest){
+        return res.status(400).json({
+            message:"friends request already exist"
+        })
+
+    }
+    const friendRequest=await FriendRequest.create({
+        sender:myId,
+        recipient:recipientId
+    })
+    res.status(201).json(friendRequest);
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong "});
+    }
+}
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoutes.js";
-import { getRecommendedUsers,getMyFriends } from "../controllers/user.controller.js";
+import { getRecommendedUsers,getMyFriends,sendFriendRequest } from "../controllers/user.controller.js";
 const router=express.Router();
 router.use(protectRoute);
 router.get("/",getRecommendedUsers);
 router.get("/friends",getMyFriends);
+router.post("/friend-request/:id",sendFriendRequest);
 
 
 
